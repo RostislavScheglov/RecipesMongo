f5c28cc29f88dfdcdb509bc32aa375523e43e8f0
Rostislav <rostislav7333@gmail.com>
2022-10-27 03:51:06 +0300

    Implemented upload\delete img, added prettier

--- server/index.js
+++ server/index.js
@@ -1,69 +1,88 @@
-import express from 'express';
-import { config } from './config/config.js';
-import mongoose from 'mongoose';
-import { registrValidation, loginValidation, recipeValidation } from './validators/authValidator.js';
-import {registration, login, getMe} from './routes/userActions.js';
-import {create, getAll, getOne, remove, update, getFavourite, getMyRecipes, getBySearch} from './routes/recipesActions.js'
-import checkSession from './middleware/checkSession.js';
-import cors from 'cors';
-import multer from 'multer';
-import fs from 'fs'
+import express from 'express'
+import bodyParser from 'body-parser'
+import { config } from './config/config.js'
+import mongoose from 'mongoose'
+import {
+  registrValidation,
+  loginValidation,
+  recipeValidation,
+} from './validators/authValidator.js'
+import { registration, login, getMe } from './routes/userActions.js'
+import {
+  create,
+  getAll,
+  getOne,
+  remove,
+  update,
+  getFavourite,
+  getMyRecipes,
+  getBySearch,
+  deleteImg,
+} from './routes/recipesActions.js'
+import checkSession from './middleware/checkSession.js'
+import cors from 'cors'
+import multer from 'multer'
 
-const app = express();
-const port = config.serverPort;
-const dbUrl = config.dbURL;
+const app = express()
+const port = config.serverPort
+const dbUrl = config.dbURL
+
 const storage = multer.diskStorage({
-    destination: (req, file, cb)=>{
-        cb(null, 'uploads')
-    },
-    filename: (req, file, cb)=>{
-        cb(null, Math.floor((Math.random()*999)) + Date.now() + file.mimetype.replace('/', '.'))
-    }
+  destination: (req, file, cb) => {
+    cb(null, 'uploads')
+  },
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      Math.floor(Math.random() * 999) +
+        Date.now() +
+        file.mimetype.replace('/', '.')
+    )
+  },
 })
 
-const upload = multer({storage})
-app.use(express.json());
-app.use(cors());
+export const upload = multer({ storage })
 
-app.post('/upload', upload.single('img'), (req, res)=>{
-    res.json({
-        url: `/uploads/${req.file.filename}`
-    })
-})
-app.delete('/upload', (req, res)=>{
-    fs.unlink(`./uploads/${req}`, (err)=>{
-    console.log(err);
-   })
-    res.json({
-        url: 'successss'
-    })
-})
+app.use(express.json())
+app.use(cors())
+
+app.delete('/:path/:id', deleteImg)
 app.use('/uploads', express.static('uploads'))
+app.post('/auth/registration', registrValidation, registration)
+app.post('/auth/login', loginValidation, login)
+app.post('/auth/me', checkSession, getMe)
+app.get('/recipes', getAll)
+app.get('/search', getBySearch)
 
-app.post('/auth/registration', registrValidation, registration);
-app.post('/auth/login', loginValidation, login);
-app.post('/auth/me', checkSession, getMe);
-app.get('/recipes', getAll);
-app.get('/search',  getBySearch);
-app.post('/recipes', checkSession, recipeValidation, create);
-app.get('/recipes/favourites', checkSession, getFavourite);
-app.get('/recipes/myrecipes', checkSession, getMyRecipes);
-app.get('/recipes/:id', getOne);
-app.delete('/recipes/:id', checkSession, remove);
-app.patch('/recipes/:id', checkSession, update);
+app.post(
+  '/recipes',
+  checkSession,
+  upload.fields([{ name: 'img', maxCount: 1 }]),
+  recipeValidation,
+  create
+)
 
-
+app.get('/recipes/favourites', checkSession, getFavourite)
+app.get('/recipes/myrecipes', checkSession, getMyRecipes)
+app.get('/recipes/:id', getOne)
+app.delete('/recipes/:id', checkSession, remove)
+app.patch('/recipes/:id', checkSession, update)
 
 const start = async () => {
-    mongoose.connect(dbUrl)
-        .then(() => { console.log("Db connected") })
-        .catch((err) => { console.log("Db error: " + err) })
-    app.listen(port, (err)=> {
-        if(err){
-            return console.log(err);
-        }
-        console.log('Server running on port ' + port);
-    });
+  mongoose
+    .connect(dbUrl)
+    .then(() => {
+      console.log('Db connected')
+    })
+    .catch((err) => {
+      console.log('Db error: ' + err)
+    })
+  app.listen(port, (err) => {
+    if (err) {
+      return console.log(err)
+    }
+    console.log('Server running on port ' + port)
+  })
 }
 
-start();
+start()
--- server/routes/recipesActions.js
+++ server/routes/recipesActions.js
@@ -1,184 +1,220 @@
-import recipeModel from '../models/Recipe.js';
-import mongoose from 'mongoose';
-import { validationResult } from 'express-validator';
+import recipeModel from '../models/Recipe.js'
+import mongoose from 'mongoose'
+import { validationResult } from 'express-validator'
+import fs from 'fs'
+import { upload } from '../index.js'
+
+// app.post('/upload', upload.single('img'), (req, res)=>{
+//     res.json({
+//         url: `/uploads/${req.file.filename}`
+//     })
+// })
+
+// app.delete('/upload', (req, res) => {
+//   fs.unlink(`./${req}`, (err) => {
+//     console.log(err)
+//   })
+//   res.json({
+//     url: 'successss',
+//   })
+// })
+
+export const deleteImg = async (req, res) => {
+  console.log(req.url)
+  fs.unlink(`.${req.url}`, (err) => {
+    console.log(err)
+  })
+  res.send({
+    url: 'Success',
+  })
+}
 
 export const getAll = async (req, res) => {
-    try{
-        const recipes = await recipeModel.find().populate('author').exec();
-        if(recipes !== null && recipes !== undefined){
-           return res.send(recipes);
-        }
-        res.send('No recipes was found')
-    }catch(err){
-        console.log(err);
-        res.status(400).json({
-            message: 'Cant get recipes'
-        })
+  try {
+    const recipes = await recipeModel.find().populate('author').exec()
+    if (recipes !== null && recipes !== undefined) {
+      return res.send(recipes)
     }
+    res.send('No recipes was found')
+  } catch (err) {
+    console.log(err)
+    res.status(400).json({
+      message: 'Cant get recipes',
+    })
+  }
 }
 
 export const getFavourite = async (req, res) => {
-    try{
-        const recipes = await recipeModel.find({likedBy:{'$in':[req.userId]}}).populate('author').exec();
-        if(recipes !== null && recipes !== undefined){
-           return res.send(recipes);
-        }
-        res.send('No recipes was found')
-    }catch(err){
-        console.log(err);
-        res.status(400).json({
-            message: 'Cant get favourite recipes'
-        })
+  try {
+    const recipes = await recipeModel
+      .find({ likedBy: { $in: [req.userId] } })
+      .populate('author')
+      .exec()
+    if (recipes !== null && recipes !== undefined) {
+      return res.send(recipes)
     }
+    res.send('No recipes was found')
+  } catch (err) {
+    console.log(err)
+    res.status(400).json({
+      message: 'Cant get favourite recipes',
+    })
+  }
 }
 
 export const getBySearch = async (req, res) => {
-    try{
-        const recipes = await recipeModel.find({title: {'$regex': req.body.search}}).populate('author').exec();
-        if(recipes !== null && recipes !== undefined){
-           return res.send(recipes);
-        }
-        res.send('No recipes was found')
-    }catch(err){
-        console.log(err);
-        res.status(400).json({
-            message: 'Cant get favourite recipes'
-        })
+  try {
+    const recipes = await recipeModel
+      .find({ title: { $regex: req.body.search } })
+      .populate('author')
+      .exec()
+    if (recipes !== null && recipes !== undefined) {
+      return res.send(recipes)
     }
+    res.send('No recipes was found')
+  } catch (err) {
+    console.log(err)
+    res.status(400).json({
+      message: 'Cant get favourite recipes',
+    })
+  }
 }
 
 export const getMyRecipes = async (req, res) => {
-    try{
-        const convertedId = mongoose.Types.ObjectId(req.userId);
-        const recipes = await recipeModel.find({author: convertedId});
-        if(recipes !== null && recipes !== undefined){
-           return res.send(recipes);
-        }
-        res.send(recipes)
-    }catch(err){
-        console.log(err);
-        res.status(400).json({
-            message: 'Cant get my recipes'
-        })
+  try {
+    const convertedId = mongoose.Types.ObjectId(req.userId)
+    const recipes = await recipeModel.find({ author: convertedId })
+    if (recipes !== null && recipes !== undefined) {
+      return res.send(recipes)
     }
+    res.send(recipes)
+  } catch (err) {
+    console.log(err)
+    res.status(400).json({
+      message: 'Cant get my recipes',
+    })
+  }
 }
 
-
 export const getOne = async (req, res) => {
-    try{
-        recipeModel.findOneAndUpdate({
-            _id: req.params.id
-        },
-        {
-            $inc: {viewsCount: 1}
-        },
-        {
-            returnDocument: 'after'
-        },
-        (err, doc)=>{
-            if(err){
-                console.log(err);
-                return res.status(400).json({
-                    message: 'Cant get recipe'
-                })
-            }
-            if(!doc){
-                return res.status(404).json({
-                    message: 'Cant find recipe'
-                })
-            }
-            res.send(doc);
-        })
-    }catch(err){
-        console.log(err);
-        res.status(500).json({
-            message: 'Cant get recipe'
-        })
-    }
+  try {
+    recipeModel.findOneAndUpdate(
+      {
+        _id: req.params.id,
+      },
+      {
+        $inc: { viewsCount: 1 },
+      },
+      {
+        returnDocument: 'after',
+      },
+      (err, doc) => {
+        if (err) {
+          console.log(err)
+          return res.status(400).json({
+            message: 'Cant get recipe',
+          })
+        }
+        if (!doc) {
+          return res.status(404).json({
+            message: 'Cant find recipe',
+          })
+        }
+        res.send(doc)
+      }
+    )
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({
+      message: 'Cant get recipe',
+    })
+  }
 }
 
 export const create = async (req, res) => {
-    try{
-        const errors = validationResult(req)
-        if(!errors.isEmpty()){
-            return res.status(400).json(errors);
-        }
-        const recipeExists = await recipeModel.findOne({title:req.body.title})
-        if(recipeExists){
-            return res.status(400).json({
-                message: "Recipe with this title exists"
-            })
-        }
-        const doc = new recipeModel({
-            title: req.body.title,
-            recipeImage: req.body.recipeImage,
-            ingredients: req.body.ingredients,
-            description: req.body.description,
-            author: req.userId,  
-        })
-       const post = await doc.save();
-       res.send(post);
-    }catch(err){
-        console.log(err);
-        res.status(400).json({
-            message: 'Cant create recipe'
-        })
+  try {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors)
     }
+    const recipeExists = await recipeModel.findOne({ title: req.body.title })
+    if (recipeExists) {
+      return res.status(400).json({
+        message: 'Recipe with this title exists',
+      })
+    }
+    const imgUrl = req.files.img[0].path.replace('\\', '/')
+    const doc = new recipeModel({
+      title: req.body.title,
+      recipeImage: imgUrl,
+      ingredients: req.body.ingredients,
+      description: req.body.description,
+      author: req.userId,
+    })
+    const post = await doc.save()
+    res.send(post)
+  } catch (err) {
+    console.log(err)
+    res.status(400).json({
+      message: 'Cant create recipe',
+    })
+  }
 }
 
 export const remove = async (req, res) => {
-    try{
-        recipeModel.findOneAndDelete({_id: req.params.id},
-            (err, doc) => {
-                if(err){
-                    console.log(err);
-                    return res.status(400).json({
-                        message: 'Cant delete recipe'
-                    })
-                }
-                if(!doc){
-                    return res.status(404).json({
-                        message: 'Cant find recipe'
-                    })
-                }
-            })
-        res.send('Recipe was successfully deleted');
-    }catch(err){
-        console.log(err);
-        res.status(400).json({
-            message: 'Cant delete recipe'
+  try {
+    recipeModel.findOneAndDelete({ _id: req.params.id }, (err, doc) => {
+      if (err) {
+        console.log(err)
+        return res.status(400).json({
+          message: 'Cant delete recipe',
         })
-    }
+      }
+      if (!doc) {
+        return res.status(404).json({
+          message: 'Cant find recipe',
+        })
+      }
+    })
+    res.send('Recipe was successfully deleted')
+  } catch (err) {
+    console.log(err)
+    res.status(400).json({
+      message: 'Cant delete recipe',
+    })
+  }
 }
 
 export const update = async (req, res) => {
-    try{
-        recipeModel.findOneAndUpdate({_id: req.params.id},
-            {
-                title: req.body.title,
-                recipeImage: req.body.recipeImage,
-                ingredients: req.body.ingredients,
-                description: req.body.description,
-                $push: {likedBy: req.body.userId}, 
-            },
-            (err, doc) => {
-                if(err){
-                    console.log(err);
-                    return res.status(400).json({
-                        message: 'Cant update recipe'
-                    })
-                }
-                if(!doc){
-                    return res.status(404).json({
-                        message: 'Cant find recipe'
-                    })
-                }
-            })
-        res.send('Recipe was successfully Updated');
-    }catch(err){
-        console.log(err);
-        res.status(400).json({
-            message: 'Cant update recipe'
-        })
-    }
+  try {
+    recipeModel.findOneAndUpdate(
+      { _id: req.params.id },
+      {
+        title: req.body.title,
+        recipeImage: req.body.recipeImage,
+        ingredients: req.body.ingredients,
+        description: req.body.description,
+        $push: { likedBy: req.body.userId },
+      },
+      (err, doc) => {
+        if (err) {
+          console.log(err)
+          return res.status(400).json({
+            message: 'Cant update recipe',
+          })
+        }
+        if (!doc) {
+          return res.status(404).json({
+            message: 'Cant find recipe',
+          })
+        }
+      }
+    )
+    res.send('Recipe was successfully Updated')
+  } catch (err) {
+    console.log(err)
+    res.status(400).json({
+      message: 'Cant update recipe',
+    })
+  }
 }
