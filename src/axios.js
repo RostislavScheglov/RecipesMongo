2f91def8316c2ba34457565b9ade930ceb9498cb
Rostislav <rostislav7333@gmail.com>
2023-07-09 20:52:25 +0200

    deployment domain changes

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-export const domain = 'http://localhost:4000/'
+export const domain = 'https://recipesapii.vercel.app/'
+
 const instance = axios.create({
   baseURL: domain,
 })
